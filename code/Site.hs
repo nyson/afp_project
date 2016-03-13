99506bb069e7645ae9c67542661265508d2d1b89
Jonathan Skårstedt <jonathan.skarstedt@gmail.com>
2016-03-13 22:00:35 +0100

    added neat ui events

--- code/Site.hs
+++ code/Site.hs
@@ -1,9 +1,25 @@
-import Toolbox
+{-# LANGUAGE OverloadedStrings #-}
+-- import Toolbox
 import Haste
-
-
+import Haste.DOM
+import Haste.Events
+import Control.Monad
 
 main = do
-  putStrLn "Welcome to Lambda-Webbox!"
+  withElems ["log", "alpha", "beta", "eta", "lambda-expression"] $
+    \[log, a, b, e, lamex] -> do
+      lamex `onEvent` KeyDown $ \k -> when (k == 13) $ do
+        Just v <- getValue lamex
+        logEntry <- newElem "li" `with` ["textContent" =: v]
+        logEntry `onEvent` Click $ \e -> do
+          putStrLn "Setting value..."
+          setProp lamex "value" v
+            
+        log `appendChild` logEntry
+          
+      a `onEvent` Click $ \e -> putStrLn "Performing alpha..."
+      b `onEvent` Click $ \e -> putStrLn "Performing beta..."
+      e `onEvent` Click $ \e -> putStrLn "Performing eta..."
+    
 
   
